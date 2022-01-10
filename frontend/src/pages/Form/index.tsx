c9e57962983c13e57a985fdfc95de372da5f83f6
Leonardo Barboza Borges <54484227+leonbborges@users.noreply.github.com>
2022-01-10 14:34:02 -0300

    Routes

---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,6 @@
+function Form(){
+    return(
+        <h1>pagina de fomulario</h1>
+    );
+}
+export default Form;
---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,6 @@
+function Listing(){
+    return(
+        <h1>pagina listada</h1>
+    );
+}
+export default Listing;
