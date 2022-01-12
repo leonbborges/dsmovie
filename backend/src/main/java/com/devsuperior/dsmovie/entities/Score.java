b5c32ac9f5443f319044a1a9f00425590b6c4ff4
Leonardo Barboza Borges <54484227+leonbborges@users.noreply.github.com>
2022-01-12 11:48:06 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/Score.java
@@ -0,0 +1,42 @@
+package com.devsuperior.dsmovie.entities;
+
+import com.devsuperior.dsmovie.entities.pk.ScorePK;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "tb_score")
+public class Score implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @EmbeddedId
+   private ScorePK id = new ScorePK();
+   private Double value;
+
+    public Score() {
+    }
+    public void setMovie(Movie movie){
+        id.setMovie(movie);
+    }
+    public void setUser(User user){
+        id.setUser(user);
+    }
+    public ScorePK getId() {
+        return id;
+    }
+
+    public void setId(ScorePK id) {
+        this.id = id;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+}
