4e04ce248a15066047e76b5a7d323eeff8a1d52d
Leonardo Barboza Borges <54484227+leonbborges@users.noreply.github.com>
2022-01-14 10:04:01 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,16 @@
+import axios from 'axios';
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { BASE_URL } from 'utils/requests';
 
 function Listing() {
+
+    //forma errada
+    axios.get(`${BASE_URL}/movies?size+128page=0`)
+        .then(Response => {
+            console.log(Response.data);
+        })
+
     return (
         <>
             <Pagination />
@@ -31,7 +40,7 @@ function Listing() {
                     <div className="col-sm-6 col-lg-4 col=xl-3 mb-3">
                         <MovieCard />
                     </div>
-                
+
                 </div>
             </div>
         </>
