1d0b6f5efcc63458f14b6bab973390a59767feea
Leonardo Barboza Borges <54484227+leonbborges@users.noreply.github.com>
2022-01-14 10:59:31 -0300

    Props

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -1,15 +1,14 @@
 import MovieScore from "components/MovieScore";
 import { Link } from "react-router-dom";
+import { Movie } from 'types/movie';
 
-const movie = {
-    id: 1,
-    image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-    title: "The Witcher",
-    count: 2,
-    score: 4.5
-};
+type props = {
+    movie: Movie;
+}
 
-function MovieCard() {
+
+
+function MovieCard({ movie }: props) {
     return (
         <div>
             <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
@@ -17,10 +16,10 @@ function MovieCard() {
                 <h3>{movie.title}</h3>
                 <MovieScore />
 
-                <Link to={`/form/${movie.id}` }>
-                <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+                <Link to={`/form/${movie.id}`}>
+                    <div className="btn btn-primary dsmovie-btn">Avaliar</div>
                 </Link>
-                
+
             </div>
         </div>
     );
--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -7,51 +7,37 @@ import { BASE_URL } from 'utils/requests';
 
 function Listing() {
 
-
+    const [page, setPage] = useState<MoviePage>({
+        content: [],
+        last: true,
+        totalPages: 0,
+        totalElements: 0,
+        size: 12,
+        number: 0,
+        first: true,
+        numberOfElements: 0,
+        empty: true
+    });
     const [pageNumber, setPageNumber] = useState(0);
     useEffect(() => {
-        axios.get(`${BASE_URL}/movies?size+128page=0`)
+        axios.get(`${BASE_URL}/movies?size+128page=${pageNumber}`)
             .then(Response => {
                 const data = Response.data as MoviePage
-                setPageNumber(data.number);
-                console.log(data);
+                setPage(data);
             })
-    }, []);
-
-
-    //forma errada
+    }, [pageNumber]);
 
     return (
         <>
-            <p>{pageNumber}</p>
             <Pagination />
             <div className="container">
                 <div className="row">
-                    <div className="col-sm-6 col-lg-4 col=xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col=xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col=xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col=xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col=xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col=xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col=xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col=xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-
+                    {page.content.map(movie => (
+                        <div key={movie.id} className="col-sm-6 col-lg-4 col=xl-3 mb-3">
+                            <MovieCard movie={movie} />
+                        </div>
+                    )
+                    )}
                 </div>
             </div>
         </>
